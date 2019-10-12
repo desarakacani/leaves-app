9d605d49b41e5a78d28de57a8a38f91084b40e66
kacanidesara <kacani.desara@gmail.com>
2019-10-12 17:20:06 +0200

    Leaves app
    - changed utils to use the roleenum

--- src/app/util/utils.ts
+++ src/app/util/utils.ts
@@ -1,8 +1,9 @@
 import {User} from '../models/user';
+import {RoleEnum} from './role.enum';
 
 export const isHr = (user: User) => {
-  return user.role === 'HR';
+  return user.role === RoleEnum.HR;
 }
 export const isManager = (user: User) => {
-  return user.role === 'manager';
+  return user.role === RoleEnum.MANAGER;
 }
