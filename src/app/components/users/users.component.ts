32feba59cf82457a7f71072bd8f846447c4a13d6
kacanidesara <kacani.desara@gmail.com>
2019-10-14 13:23:12 +0200

    Leaves app
    -added user list
    
    -added user create
    
    -added validation to user form
    
    -added edit user
    
    -added updated user
    
    -added delete user

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,9 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
-import {BackendService} from '../../services/backend.service';
-import {FormGroup} from '@angular/forms';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {User} from '../../models/user';
 import {UserService} from '../../services/user.service';
+import {ToastrService} from 'ngx-toastr';
+import {RoleEnum} from '../../util/role.enum';
 
 @Component({
   selector: 'app-users',
@@ -12,20 +13,33 @@ import {UserService} from '../../services/user.service';
 })
 export class UsersComponent implements OnInit {
   closeResult: string;
+  submitted = false;
   userForm: FormGroup;
   user: User;
+  selectedUserToEdit: User = null;
   private users: User[];
 
+  private roles = [RoleEnum.USER, RoleEnum.MANAGER];
+
   constructor(
-    private backend: BackendService,
     private modalService: NgbModal,
-    private userService: UserService) {
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private toastr: ToastrService) {
   }
 
   ngOnInit() {
-    this.backend.getUsers().subscribe(users => {
-      console.log(users[0].first_name);
-      this.users = users;
+
+    this.userForm = this.formBuilder.group({
+      first_name: ['', Validators.required],
+      last_name: ['', Validators.required],
+      email: ['', Validators.required],
+      role: [null, Validators.required],
+      gender: [null, Validators.required],
+      password: ['', Validators.required]
+    });
+    this.userService.getUsers().subscribe(users => {
+      this.users = users.filter(user => user.role !== RoleEnum.HR);
     });
   }
 
@@ -33,26 +47,69 @@ export class UsersComponent implements OnInit {
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      this.submitted = false;
+      this.userForm.reset();
     });
   }
 
   onSubmit() {
-
+    this.submitted = true;
+    if (this.userForm.invalid) {
+      return;
+    }
     this.user = Object.assign({}, this.userForm.value);
-    console.log(this.user);
-    this.userService.addUser(this.user).subscribe(_ => {
+    this.userService.addUser(this.user).subscribe(addedUser => {
+      this.submitted = false;
       this.modalService.dismissAll();
+      this.toastr.success('Added with success!', 'Success');
+      this.users.push(addedUser as User);
     });
   }
 
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
+  edit(user: User, content) {
+    this.selectedUserToEdit = user;
+    this.userForm.get('first_name').patchValue(user.first_name);
+    this.userForm.get('last_name').patchValue(user.last_name);
+    this.userForm.get('email').patchValue(user.email);
+    this.userForm.get('role').patchValue(user.role);
+    this.userForm.get('gender').patchValue(user.gender);
+    this.userForm.get('password').patchValue(user.password);
+
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.submitted = false;
+      this.selectedUserToEdit = null;
+      this.userForm.reset();
+    });
+  }
+
+  update() {
+    this.submitted = true;
+    if (this.userForm.invalid) {
+      return;
     }
+    this.selectedUserToEdit.first_name = this.userForm.get('first_name').value;
+    this.selectedUserToEdit.last_name = this.userForm.get('last_name').value;
+    this.selectedUserToEdit.email = this.userForm.get('email').value;
+    this.selectedUserToEdit.role = this.userForm.get('role').value;
+    this.selectedUserToEdit.gender = this.userForm.get('gender').value;
+    this.selectedUserToEdit.password = this.userForm.get('password').value;
+    this.userService.updateUser(this.selectedUserToEdit).subscribe(_ => {
+      this.modalService.dismissAll();
+      this.toastr.success('updated successfully');
+    });
+  }
+
+  delete(user: User) {
+    this.userService.delete(user).subscribe(_ => {
+      this.toastr.success('User deleted with success!');
+      const index = this.users.indexOf(user);
+      this.users.splice(index, 1);
+    });
+  }
+
+  get f() {
+    return this.userForm.controls;
   }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {User} from '../models/user';
 import {environment} from '../../environments/environment';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,17 @@ export class UserService {
 
   addUser(user: User) {
     return this.httpClient.post(`${environment.baseUrl}/users`, user);
+  }
+
+  getUsers(): Observable<User[]> {
+    return this.httpClient.get<User[]>(`${environment.baseUrl}/users`);
+  }
+
+  updateUser(user: User) {
+    return this.httpClient.patch(`${environment.baseUrl}/users/${user.id}`, user);
+  }
+
+  delete(user: User) {
+    return this.httpClient.delete(`${environment.baseUrl}/users/${user.id}`);
   }
 }
