0029768220459a9e32e3a1853480da3812005e4c
kacanidesara <kacani.desara@gmail.com>
2019-10-15 14:29:58 +0200

    Leaves app
    -removed comment

--- src/app/components/requests/requests.component.ts
+++ src/app/components/requests/requests.component.ts
@@ -93,7 +93,6 @@ export class RequestsComponent implements OnInit {
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
-      // this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
     });
   }
 }
